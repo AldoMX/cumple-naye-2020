0903f17fb823dd8f6adbac96856dbbd2bb118249
Aldo Fregoso <aldomx@gmail.com>
2020-03-28 02:44:14 -0600

    Updates

---
+++ precache-manifest.a6bd22fbd96aa514e551229923c29ed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9caa878b5c7c365808b72d6882fd83d8",
+    "url": "/cumple-naye-2020/index.html"
+  },
+  {
+    "revision": "a5291826eea88828ae55",
+    "url": "/cumple-naye-2020/static/css/2.3beecd4b.chunk.css"
+  },
+  {
+    "revision": "e53e6bed26522c9684d0",
+    "url": "/cumple-naye-2020/static/css/main.85661999.chunk.css"
+  },
+  {
+    "revision": "a5291826eea88828ae55",
+    "url": "/cumple-naye-2020/static/js/2.78b6890d.chunk.js"
+  },
+  {
+    "revision": "af0a063b7aacc833132980c2224e7d2e",
+    "url": "/cumple-naye-2020/static/js/2.78b6890d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53e6bed26522c9684d0",
+    "url": "/cumple-naye-2020/static/js/main.32986353.chunk.js"
+  },
+  {
+    "revision": "45f0cd73e3c298f232f5",
+    "url": "/cumple-naye-2020/static/js/runtime-main.c8e3eab6.js"
+  }
+]);
